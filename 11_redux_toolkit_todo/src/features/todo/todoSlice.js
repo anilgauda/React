ff4d03f0af28ef79e02bcd977d82affde751ba0a
Anil Gauda <anil2993@gmail.com>
2024-03-01 20:30:58 +0000

    todo list with redux

--- 11_redux_toolkit_todo/src/features/todo/todoSlice.js
+++ 11_redux_toolkit_todo/src/features/todo/todoSlice.js
@@ -11,18 +11,18 @@ export const todoSlice = createSlice({
         addTodo: (state,action) => {
             const todo = {
                 id: nanoid(),
-                text: action.payload.text,
+                text: action.payload,
             }
             state.todos.push(todo)
         },
         removeTodo: (state,action) =>{
-            state.todos =  state.todos.filter((todo) => todo.id!==action.payload.id)
+            state.todos =  state.todos.filter((todo) => todo.id!==action.payload)
         },
         updateTodo: (state,action) =>{
-            state.todos = state.todos.map((todo)=>(todo.id===action.payload.id)? todo.text= action.payload.text : todo.text)
+            state.todos = state.todos.map((todo)=>(todo.id===action.payload)? todo.text= action.payload : todo.text)
         },
         toggleCompleted: (state,action) =>{
-            state.todos = state.todos.map((todo)=>(todo.id=== action.payload.id)? todo.completed = true: todo.completed)
+            state.todos = state.todos.map((todo)=>(todo.id=== action.payload)? todo.completed = true: todo.completed)
         }
     }
 })
