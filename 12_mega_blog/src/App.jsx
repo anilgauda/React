4714fd315a36f15dd4483f1d5a46d8fe0f5b6b73
anilgauda <anil2993@gmail.com>
2024-03-05 17:37:41 +0000

    Added Redux store

--- 12_mega_blog/src/App.jsx
+++ 12_mega_blog/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login,logout } from './store/authSlice';
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-<>
-    <h1> A blog with Appwrite</h1>
-</>
-  )
+  const [loading, setLoading] =useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData) =>{
+      if(userData) {
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    }).finally(() => setLoading(false))
+  },[]);
+
+  return loading ? null:(<div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div>)
 }
 
 export default App
