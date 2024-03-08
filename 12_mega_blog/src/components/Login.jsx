63345c94c695e52c1d2d4136c04dd0c5d777be57
anilgauda <anil2993@gmail.com>
2024-03-08 17:07:03 +0000

    Added new components for Auth and login

--- 12_mega_blog/src/components/Login.jsx
+++ 12_mega_blog/src/components/Login.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
 import {login as storeLogin} from '../store/authSlice'
 import authService from '../appwrite/auth';
+import Input from './Input';
+import Button from './Button';
 
 function Login() {
     const navigate = useNavigate();
@@ -48,7 +50,16 @@ function Login() {
         </p>
         {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
         <form onSubmit={handleSubmit(login)} className='mt-8'>
-
+        <div className='space-y-5'>
+            <Input label="Email: " type ='email' placeholder= "Enter your email" {...register("email",{required:true, validate:{
+                matchPattern:(value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Email address must be valid",
+            }})}/>
+            <Input label= "Password: " type="password" placeholder="Enter your Password" {...register("password",{required:true,})}/>
+            <Button
+            type='submit'
+            className='w-full'
+            >Sign in </Button>
+        </div>
         </form>
         </div>
         </div>
