3367dd78604a4fae6e5c6ee822b9977fe98f58a0
Anil Gauda <anil2993@gmail.com>
2024-03-10 15:57:08 +0000

    Added pages and RTE component

---
+++ 12_mega_blog/src/pages/AddPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react' 
+import { Container,PostForm } from '../components'
+
+function AddPost() {
+  return (
+    <div className='py-8'><Container><AddPostComponent/></Container></div>
+  )
+}
+
+export default AddPost
---
+++ 12_mega_blog/src/pages/EditPost.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import {Container, PostForm} from "../components"
+import { useNavigate, useParams } from 'react-router-dom'
+import service from '../appwrite/config'
+function EditPost() {
+    const [post,setPost] =useState([])
+    const {slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        if(slug) {
+            service.getPost(slug).then(post =>{
+                if(post) {
+                    setPost(post)
+                } else {
+                    navigate("/")
+                }
+            })
+        }
+    },[slug,navigate])
+  return post? (<div className='py-8'>
+    <Container>
+        <PostForm post={post}/>
+    </Container>
+  </div>):null
+}
+
+export default EditPost
